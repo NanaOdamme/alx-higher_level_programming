464e77f51a8d1e940b2eb469a770005cd60b3b55
NanaOdamme <eronastyles@gmail.com>
2023-10-03 07:01:54 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for char_code in range(ord('z'), ord('a') - 1, -1):
-    char = chr(char_code)
-    if (char_code - ord('z')) % 2 == 0:
-        print(char.lower(), end='')
-    else:
-        print(char.upper(), end='')
+for char_code in range(ord('z'), ord('A') - 1, -1):
+    print("{}".format(chr(char_code)), end='')
