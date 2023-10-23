0fe4e473aae589e08272c83ed9fa24de04a5ea2d
NanaOdamme <eronastyles@gmail.com>
2023-10-23 05:31:46 -0700

    task0 corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            num += 1
-        except IndexError:
-            break
-    print("")
-    Return
+    # Initialize a variable to count the elements printed
+    elements_printed = 0
+
+    try:
+        for i in range(x):
+            # Try to print the element if it exists
+            print(my_list[i], end=' ')
+            elements_printed += 1
+    except IndexError:
+        # Handle the IndexError when x is greater than the length of my_list
+        pass
+
+    # Print a new line
+    print()
+
+    return elements_printed
+
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+x = 3
+printed_count = safe_print_list(my_list, x)
+print(f"Number of elements printed: {printed_count}")
