ffa160c0076d059bbba75422cb9ba0874cf362e8
NanaOdamme <eronastyles@gmail.com>
2023-11-06 04:18:00 -0800

    more corrections

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+    This module returns the list of available attributes
+    and methods of an object
+"""
 
 
 def lookup(obj):
-    # Use the dir() function to get a list of attributes and methods of object
+    """Use the dir() function to get list attributes and methods of object"""
     attributes_and_methods = dir(obj)
 
     return attributes_and_methods
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""This module inherits from the list class"""
 
 
 class MyList(list):
     def print_sorted(self):
-        # Sort the list in ascending order
+        """Sort the list in ascending order"""
         sorted_list = sorted(self)
 
-        # Print the sorted list
+        """Print the sorted list"""
         print(sorted_list)
